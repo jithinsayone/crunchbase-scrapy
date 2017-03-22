cc93b6e03a05859da1fb50b4362f2eb8c08b577c
jithinsayone <jithin.sayone@gmail.com>
2017-03-22 14:29:45 +0530

    ADMIN Company search

--- crunchbase/api/admin.py
+++ crunchbase/api/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import People_Crunchbase,Company_Crunchbase
 
+class Company_CrunchbaseAdmin(admin.ModelAdmin):
+    list_display=( 'name','category')
+    search_fields = [ 'name','category']
+
+
+
 admin.site.register(People_Crunchbase)
-admin.site.register(Company_Crunchbase)
+admin.site.register(Company_Crunchbase,Company_CrunchbaseAdmin)
+
+
 
 # Register your models here.
