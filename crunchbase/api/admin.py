b92dca38f82b1ff41b474531ac935f135b8bac3d
jithinsayone <jithin.sayone@gmail.com>
2017-03-22 18:21:40 +0530

    admin modification

--- crunchbase/api/admin.py
+++ crunchbase/api/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import People_Crunchbase,Company_Crunchbase
 
 class Company_CrunchbaseAdmin(admin.ModelAdmin):
-    list_display=( 'name','category')
-    search_fields = [ 'name','category']
+    list_display=( 'name','category','location')
+    search_fields = [ 'name','category','location']
 
 class People_CrunchbaseAdmin(admin.ModelAdmin):
-    list_display=( 'name','company_name')
-    search_fields = [ 'name','company_name']
+    list_display=( 'name','company_name','designation','location')
+    search_fields = [ 'name','company_name','designation','location']
 
 admin.site.register(People_Crunchbase,People_CrunchbaseAdmin)
 admin.site.register(Company_Crunchbase,Company_CrunchbaseAdmin)
