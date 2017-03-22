1b01c37234b44d787f0162ba7eded22e8556bf6a
jithinsayone <jithin.sayone@gmail.com>
2017-03-22 17:09:14 +0530

    Add filter in API

--- crunchbase/api/views.py
+++ crunchbase/api/views.py
@@ -26,20 +26,23 @@ class CompanySave(APIView):
 
 
       try:
-          obj_company=Company_Crunchbase()
-          obj_company.name = request.data["name"]
-          obj_company.description = request.data["description"]
-          obj_company.rank = request.data["rank"]
-          obj_company.category  = request.data["category"]
-          obj_company.location = request.data["location"]
-          obj_company.website = request.data["website"]
-          obj_company.facebook = request.data["facebook"]
-          obj_company.twitter = request.data["twitter"]
-          obj_company.linkdin = request.data["linkdin"]
-          obj_company.phone = request.data["phone"]
-          obj_company.email = request.data["email"]
-          obj_company.save()
-          return Response({'data_status':True})
+          if not Company_Crunchbase.objects.filter(name=request.data["name"]).exists():
+              obj_company=Company_Crunchbase()
+              obj_company.name = request.data["name"]
+              obj_company.description = request.data["description"]
+              obj_company.rank = request.data["rank"]
+              obj_company.category  = request.data["category"]
+              obj_company.location = request.data["location"]
+              obj_company.website = request.data["website"]
+              obj_company.facebook = request.data["facebook"]
+              obj_company.twitter = request.data["twitter"]
+              obj_company.linkdin = request.data["linkdin"]
+              obj_company.phone = request.data["phone"]
+              obj_company.email = request.data["email"]
+              obj_company.save()
+              return Response({'data_status':True})
+          else:
+              return Response({'data_status':True})
       except:
          return Response({'data_status':False})
 
@@ -58,22 +61,27 @@ class PeopleSave(APIView):
 
   def post(self,request, *args, **kwargs):
 
+
       try:
-          obj_person=People_Crunchbase()
-          obj_person.name = request.data["name"]
-          obj_person.gender = request.data["gender"]
-          obj_person.designation = request.data["designation"]
-          obj_person.first_name  = request.data["first_name"]
-          obj_person.last_name = request.data["last_name"]
-          obj_person.rank = request.data["rank"]
-          obj_person.twitter = request.data["twitter"]
-          obj_person.facebook = request.data["facebook"]
-          obj_person.company_name = request.data["company"]
-          obj_person.location = request.data["location"]
-          obj_person.linkdin = request.data["linkdin"]
-          obj_person.biography=request.data["biography"]
-          obj_person.save()
-          return Response({'data_status':True})
+          if not People_Crunchbase.objects.filter(name=request.data["name"]).exists():
+              obj_person=People_Crunchbase()
+              obj_person.name = request.data["name"]
+              obj_person.gender = request.data["gender"]
+              obj_person.designation = request.data["designation"]
+              obj_person.first_name  = request.data["first_name"]
+              obj_person.last_name = request.data["last_name"]
+              obj_person.rank = request.data["rank"]
+              obj_person.twitter = request.data["twitter"]
+              obj_person.facebook = request.data["facebook"]
+              obj_person.company_name = request.data["company"]
+              obj_person.location = request.data["location"]
+              obj_person.linkdin = request.data["linkdin"]
+              obj_person.biography=request.data["biography"]
+              obj_person.save()
+              return Response({'data_status':True})
+          else:
+              return Response({'data_status':True})
+
       except:
          return Response({'data_status':False})
 
