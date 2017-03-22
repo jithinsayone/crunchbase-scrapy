22575c61d7af1c48d92cbb852ddea6c31bc68803
jithinsayone <jithin.sayone@gmail.com>
2017-03-22 18:59:45 +0530

    Export CSV

--- crunchbase/crunchbase/settings.py
+++ crunchbase/crunchbase/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'suit',
+    'django_csv_exports',
     'api',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -71,6 +72,11 @@ TEMPLATES = [
     },
 ]
 
+# Use if you want to check user level permissions only users with the can_csv_<model_label>
+# will be able to download csv files.
+DJANGO_EXPORTS_REQUIRE_PERM = True
+# Use if you want to disable the global django admin action. This setting is set to True by default.
+DJANGO_CSV_GLOBAL_EXPORTS_ENABLED = False
 
 WSGI_APPLICATION = 'crunchbase.wsgi.application'
 
