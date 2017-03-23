b5d83633d91780fe38cad4c2d291d509f9895c26
jithinsayone <jithin.sayone@gmail.com>
2017-03-23 10:59:23 +0530

    Admin change

--- crunchbase/api/admin.py
+++ crunchbase/api/admin.py
@@ -14,13 +14,14 @@ class Company_CrunchbaseAdmin(admin.ModelAdmin):
     search_fields = [ 'name','category','location']
 
 class People_CrunchbaseAdmin(ImportExportModelAdmin):
-    list_display=( 'name','company_name','designation','location')
-    search_fields = [ 'name','company_name','designation','location']
-    list_filter = (
-       'company_name', 'designation', 'location',
+   #  list_display=( 'name')#,'company_name','designation','location')
+   #  # search_fields = [ 'name','company_name','designation','location']
+   #  list_filter = (
+   #     'company_name', 'designation', 'location',
+   #
+   # )
+   pass
 
-   )
-    resource_class = People_CrunchbaseResource
 
 class PeoplecrunchProxy(admin.ModelAdmin):
     list_display=( 'name','company_name','designation','location')
@@ -29,10 +30,12 @@ class PeoplecrunchProxy(admin.ModelAdmin):
        'company_name', 'designation', 'location',
 
    )
+    resource_class = People_CrunchbaseResource
 
 
 admin.site.register(People_Crunchbase,People_CrunchbaseAdmin)
 admin.site.register(Company_Crunchbase,Company_CrunchbaseAdmin)
+admin.site.register(People_Crunchbase,PeoplecrunchProxy)
 
 
 
