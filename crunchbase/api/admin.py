bf3df49a5910361785914c4530e32616c7121812
jithinsayone <jithin.sayone@gmail.com>
2017-03-23 11:07:03 +0530

    Admin change

--- crunchbase/api/admin.py
+++ crunchbase/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import People_Crunchbase,Company_Crunchbase
+from .models import People_Crunchbase,Company_Crunchbase,PeopleToImport
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
 
@@ -14,28 +14,24 @@ class Company_CrunchbaseAdmin(admin.ModelAdmin):
     search_fields = [ 'name','category','location']
 
 class People_CrunchbaseAdmin(ImportExportModelAdmin):
-   #  list_display=( 'name')#,'company_name','designation','location')
-   #  # search_fields = [ 'name','company_name','designation','location']
-   #  list_filter = (
-   #     'company_name', 'designation', 'location',
-   #
-   # )
-   pass
 
+    pass
 
-class PeoplecrunchProxy(admin.ModelAdmin):
-    list_display=( 'name','company_name','designation','location')
-    search_fields = [ 'name','company_name','designation','location']
-    list_filter = (
+class PeoplecrunchProxyAdmin(admin.ModelAdmin):
+
+   list_display=( 'name','company_name','designation','location')
+   search_fields = [ 'name','company_name','designation','location']
+   list_filter = (
        'company_name', 'designation', 'location',
 
    )
-    resource_class = People_CrunchbaseResource
+   resource_class = People_CrunchbaseResource
+
 
 
 admin.site.register(People_Crunchbase,People_CrunchbaseAdmin)
 admin.site.register(Company_Crunchbase,Company_CrunchbaseAdmin)
-admin.site.register(People_Crunchbase,PeoplecrunchProxy)
+admin.site.register(PeopleToImport,PeoplecrunchProxyAdmin)
 
 
 
